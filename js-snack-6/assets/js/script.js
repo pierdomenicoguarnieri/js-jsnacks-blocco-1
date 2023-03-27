467b8b3ee21eabb94d029a81d6f50514f35c3e26
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-03-27 16:42:37 +0200

    add: js-snack-6 output

--- js-snack-6/assets/js/script.js
+++ js-snack-6/assets/js/script.js
@@ -1,6 +1,23 @@
 const n = parseInt(prompt("Inserisci il numero dei numeri elevati al cubo che vorresti stampare:"));
 
+let message = "";
+
+const outputTitle = document.getElementById("outputTitle");
+
+const outputContent = document.getElementById("outputContent")
+
 for(let i = 0; i < n; i++){
   const number = Math.pow(i, 3);
 
+  message +=`
+  ${i}^3 = ${number} <br>
+  `
 }
+
+outputTitle.innerHTML =`
+Hai deciso di generare il cubo di ${n} numeri:
+`
+
+outputContent.innerHTML =`
+${message}
+`
