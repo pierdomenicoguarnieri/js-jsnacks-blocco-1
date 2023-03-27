83d53a511120a2fce7dbee455e390adbdf8fbbfb
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-03-27 17:19:05 +0200

    add: js-snack-8 array cell sum calc

--- js-snack-8/assets/js/script.js
+++ js-snack-8/assets/js/script.js
@@ -1,6 +1,12 @@
 let number;
+
+let sum = 0;
 do{
   number = prompt("Inserisci un numero di 4 cifre:");
 }while(!(number.length === 4))
 
 const array = number.split("");
+
+for(let i = 0; i < array.length; i++){
+  sum += parseInt(array[i]);
+}
