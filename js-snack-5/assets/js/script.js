37db707029350971cfd83e5cc27e3d1e4202750e
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-03-27 16:13:37 +0200

    add: js-snack-5 modulus check, output message

--- js-snack-5/assets/js/script.js
+++ js-snack-5/assets/js/script.js
@@ -1,8 +1,19 @@
 const numbers = [];
 
+const output = document.getElementById("output");
+
 let i = 0;
 
 while(i < 6){
   let number = parseInt(prompt("Inserisci un numero: (iterazione numero " + (i + 1) + " del ciclo)"));
+  
+  if(!(number % 3)){
+    numbers.push(number);
+  }
+
   i++;
 }
+
+output.innerHTML =`
+Hai inserito ${i} numeri, dei quali ${numbers} sono divisibili per 3.
+`
