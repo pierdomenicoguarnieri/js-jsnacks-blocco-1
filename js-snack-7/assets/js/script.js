93e23571176e42bd45c6fbeaa7253092c5e32e19
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-03-27 16:58:04 +0200

    add: js-snack-7 output

--- js-snack-7/assets/js/script.js
+++ js-snack-7/assets/js/script.js
@@ -1,6 +1,19 @@
+const outputTitle = document.getElementById("outputTitle");
+
+const outputContent = document.getElementById("outputContent");
+
+let message = "";
+
 for(let i = 0; i < 1000; i++){
   const number = Math.pow(2, i);
   message +=`
   2^${i} = ${number}<br>
   `
 }
+
+
+outputTitle.innerHTML = "Sono state calcolate tutte le potenze cha vanno da 2^0 a 2^999 (1000 potenze)."
+
+outputContent.innerHTML =`
+${message}
+`
