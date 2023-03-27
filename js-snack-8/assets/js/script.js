4aa7d9f735a13dd79df894993bfbf242211cc024
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-03-27 17:19:21 +0200

    add: js-snack-8 output

--- js-snack-8/assets/js/script.js
+++ js-snack-8/assets/js/script.js
@@ -1,6 +1,11 @@
 let number;
 
 let sum = 0;
+
+const outputTitle = document.getElementById("outputTitle");
+
+const outputContent = document.getElementById("outputContent");
+
 do{
   number = prompt("Inserisci un numero di 4 cifre:");
 }while(!(number.length === 4))
@@ -10,3 +15,10 @@ const array = number.split("");
 for(let i = 0; i < array.length; i++){
   sum += parseInt(array[i]);
 }
+
+outputTitle.innerHTML = "È stato diviso e sommato il numero fornito";
+
+outputContent.innerHTML =`
+Il numero ${number} è stato diviso nell'array: ${array}. <br>
+Le sue celle sono state sommate tra loro ed il risultato è: ${sum}.
+`
